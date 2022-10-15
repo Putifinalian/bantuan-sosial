54355dd0ff0236efdbf94f0dd7120a464d738415
beruangdev <beruangdev@gmail.com>
2022-10-15 17:35:43 +0700

    tesst

--- application/modules/kriteriaa/controllers/Kriteriaa.php
+++ application/modules/kriteriaa/controllers/Kriteriaa.php
@@ -7,8 +7,9 @@ class kriteriaa extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        // $this->load->model('home_model');
-        // middleware_check_user($this);
+        $this->load->model('kriteriaa_model');
+        $this->load->library('form_validation');
+        middleware_check_user($this);
     }
 
     /**
@@ -17,13 +18,49 @@ class kriteriaa extends CI_Controller
      */
     public function index()
     {
-        echo "INDEX Kriteriaa";
-        return ""; 
+        if ($this->input->method() == "post") {
+            $_method = $this->input->post('_method');
+            if ($_method == "put" || $_method == "patch") {
+                // return $this->update();
+            } else if ($_method == "delete") {
+                // return $this->delete();
+            } else {
+
+                // return $this->store();
+            }
+        }
+
+        return $this->view_criteria();
     }
 
-    public function goreng()
+    public function view_criteria()
     {
-        echo "goreng Kriteriaa";
-        return ""; 
+        $kriteria = $this->db->query("SELECT * from kriteria_bansos LEFT JOIN kriteria ON kriteria_bansos.id_kriteria = kriteria.id_kriteria GROUP BY kriteria.id_kriteria ORDER BY kriteria.id_kriteria ASC")->result_object();
+        $data['kriteria'] = $kriteria;
+
+        // return $this->json($kriteria);
+
+        $bansoss = $this->db->query("SELECT * FROM bansos")->result_object();
+        $data['bansoss'] = $bansoss;
+
+        $kategori_bansoss = $this->db->query("SELECT * FROM kategori_bansos")->result_object();
+        $data['kategori_bansoss'] = $kategori_bansoss;
+
+        // var_dump($data['kriteria']);exit;
+        $this->load->view("include/head");
+        $this->load->view("include/top-header");
+        $this->load->view('kriteria_views', $data);
+        $this->load->view("include/admin/sidebar");
+        $this->load->view("include/panel");
+        $this->load->view("include/alert");
+        $this->load->view("include/footer");
+    }
+
+    public function json($data)
+    {
+        return $this->output
+            ->set_content_type('application/json')
+            ->set_status_header(200)
+            ->set_output(json_encode($data));
     }
 }

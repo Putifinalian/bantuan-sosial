7c8ba6327d75d5f7362520f666edbb45310f8f61
beruangdev <beruangdev@gmail.com>
2022-10-15 17:42:51 +0700

    tesst

--- application/modules/kriteriaa/controllers/kriteriaa.php
+++ application/modules/kriteriaa/controllers/kriteriaa.php
@@ -7,9 +7,9 @@ class kriteriaa extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->model('kriteriaa_model');
-        $this->load->library('form_validation');
-        middleware_check_user($this);
+        // $this->load->model('kriteriaa_model');
+        // $this->load->library('form_validation');
+        // middleware_check_user($this);
     }
 
     /**
@@ -19,7 +19,7 @@ class kriteriaa extends CI_Controller
     public function index()
     {
         echo "INDEX";
-        return ""; 
+        return "";
         // if ($this->input->method() == "post") {
         //     $_method = $this->input->post('_method');
         //     if ($_method == "put" || $_method == "patch") {
@@ -35,34 +35,34 @@ class kriteriaa extends CI_Controller
         // return $this->view_criteria();
     }
 
-    public function view_criteria()
-    {
-        $kriteria = $this->db->query("SELECT * from kriteria_bansos LEFT JOIN kriteria ON kriteria_bansos.id_kriteria = kriteria.id_kriteria GROUP BY kriteria.id_kriteria ORDER BY kriteria.id_kriteria ASC")->result_object();
-        $data['kriteria'] = $kriteria;
+    // public function view_criteria()
+    // {
+    //     $kriteria = $this->db->query("SELECT * from kriteria_bansos LEFT JOIN kriteria ON kriteria_bansos.id_kriteria = kriteria.id_kriteria GROUP BY kriteria.id_kriteria ORDER BY kriteria.id_kriteria ASC")->result_object();
+    //     $data['kriteria'] = $kriteria;
 
-        // return $this->json($kriteria);
+    //     // return $this->json($kriteria);
 
-        $bansoss = $this->db->query("SELECT * FROM bansos")->result_object();
-        $data['bansoss'] = $bansoss;
+    //     $bansoss = $this->db->query("SELECT * FROM bansos")->result_object();
+    //     $data['bansoss'] = $bansoss;
 
-        $kategori_bansoss = $this->db->query("SELECT * FROM kategori_bansos")->result_object();
-        $data['kategori_bansoss'] = $kategori_bansoss;
+    //     $kategori_bansoss = $this->db->query("SELECT * FROM kategori_bansos")->result_object();
+    //     $data['kategori_bansoss'] = $kategori_bansoss;
 
-        // var_dump($data['kriteria']);exit;
-        $this->load->view("include/head");
-        $this->load->view("include/top-header");
-        $this->load->view('kriteria_views', $data);
-        $this->load->view("include/admin/sidebar");
-        $this->load->view("include/panel");
-        $this->load->view("include/alert");
-        $this->load->view("include/footer");
-    }
+    //     // var_dump($data['kriteria']);exit;
+    //     $this->load->view("include/head");
+    //     $this->load->view("include/top-header");
+    //     $this->load->view('kriteria_views', $data);
+    //     $this->load->view("include/admin/sidebar");
+    //     $this->load->view("include/panel");
+    //     $this->load->view("include/alert");
+    //     $this->load->view("include/footer");
+    // }
 
-    public function json($data)
-    {
-        return $this->output
-            ->set_content_type('application/json')
-            ->set_status_header(200)
-            ->set_output(json_encode($data));
-    }
+    // public function json($data)
+    // {
+    //     return $this->output
+    //         ->set_content_type('application/json')
+    //         ->set_status_header(200)
+    //         ->set_output(json_encode($data));
+    // }
 }
